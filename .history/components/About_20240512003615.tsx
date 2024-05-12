e1c16e464fee598ed5cf5fcc84ac365c1105865c
Abel-A16 <zerubabelendale16@gmail.com>
2024-05-12 01:59:59 -0700

    fetch experience to frontend

---
+++ .history/components/About_20240512003615.tsx
@@ -0,0 +1,61 @@
+'use client'
+import { motion } from 'framer-motion'
+import React from 'react';
+import Title from './Title';
+
+import { useEffect, useState } from 'react';
+import { PageInfo } from '@/typings';
+import { urlFor } from '@/sanity';
+
+
+  type Props = {
+    pageInfo:PageInfo
+  };
+  
+
+export default function About({pageInfo}:Props)  {
+  return (
+    <motion.div 
+    initial = {{
+        opacity: 0,
+    }}
+    transition={{
+        duration: 1.5,
+    }}
+    whileInView={{
+        opacity: 1,
+    }}
+    className='flex flex-col relative h-screen  text-center md:text-left md:flex-row 
+    max-w-7xl px-10 justify-evenly mx-auto items-center'>
+        <Title text={'About'}/>
+        <motion.img
+            initial = {{
+                x: -200,
+                opacity: 0,
+            }}
+            transition={{
+                duration: 1.2,
+            }}
+            whileInView={{
+                opacity: 1,
+                x: 0,
+            }}
+            viewport={{once: true}}
+            src={urlFor(pageInfo?.profilePic).url()}
+            className='mt-28 md:mt-0  md:mb-0 w-56 h-55 rounded-ful object-cover
+             md:rounded-sm md:w-72  sm:rounded-full xl:w-[400px] xl:h-[450px]'
+        />
+        <div className=' space-y-2 px-0 md:px-10'>
+            <h4 className='text-3xl font-semibold'>
+              Here is a{" "}
+                <span className='hover:underline hover:tracking-[0.1px] text-[#778A35] transition-opacity
+                 duration-1000 decoration-[#778A35]'>
+                    little
+                </span> background
+            </h4>
+            <p className=' text-base'>{pageInfo?.backgroundInformation}
+            </p>
+        </div>
+    </motion.div>
+  )
+}
---
+++ .history/components/ExperianceCard_20240512004608.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { Experience } from '@/typings';
+import { urlFor } from '@/sanity';
+
+type Props = {
+  experience: Experience
+}
+
+function ExperienceCard({experience}: Props) {
+  return (
+    <article className=" snap-mandatory flex flex-col rounded-lg items-center space-y-2 h-[550px] flex-shrink-0 w-[500px]
+     md:w-[600px] xl:w-[700px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 
+     cursor-pointer transition-opacity duration-200 overflow-hidden">
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+        }}
+        viewport={{ once: true }}
+        className="w-32 h-32 rounded-full xl:rounded-full object-cover object-center"
+        src={ urlFor(experience?.companyImage).url()}
+        alt=""
+      />
+      <div className=" md:ml-1 ml-1 px-0 md:px-10">
+        <h4 className="  text-2xl font-light">MEMBER OF HTECH</h4>
+        <p className="font-bold text-xl mt-1">Some text here</p>
+        <div className="flex space-x-2 mt-1">
+          {experience.technologies.map(technology => (
+            <img 
+              key={technology._id}
+              src={urlFor(technology.image).url()}
+              className='h-10 w-10 rounded-full'
+              alt=''
+            />
+          ))}
+        </div>
+        <p className="uppercase text-gray-300 py-5">Started work... - Ended...</p>
+        <ul className="list-disc space-y-4 text-lg">
+          <li>Summary pointers Summary pointers Summary pointers Summary pointers</li>
+          <li>Summary pointers Summary pointers Summary pointers Summary pointers</li>
+          <li>Summary pointers Summary pointers Summary pointers Summary pointers</li>
+          <li>Summary pointers Summary pointers Summary pointers Summary pointers</li>
+          <li>Summary pointers Summary pointers Summary pointers Summary pointers</li>
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default ExperienceCard;
